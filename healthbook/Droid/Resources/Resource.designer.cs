5a5ab8224d6d2fe67f623e99c8a2eaede4f8cca4
Atabak Sahraei <atabaksahraei@Atabaks-Macbook-Pro.local>
2015-05-01 19:01:54 +1000

    icons, multi screen

--- healthbook/Droid/Resources/Resource.designer.cs
+++ healthbook/Droid/Resources/Resource.designer.cs
@@ -49,7 +49,10 @@ namespace healthbook.Droid
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int icon = 2130837504;
+			public const int ic_launcher = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int icon = 2130837505;
 			
 			static Drawable()
 			{
