f1af92fb1ca1d5aea0c3e7f14b08454b5d2141a2
Atabak Sahraei <atabaksahraei@Atabaks-Macbook-Pro.local>
2015-05-07 00:36:18 +1000

    App Synchronised with azure

--- healthbook/healthbook/Model/BL/Manager.cs
+++ healthbook/healthbook/Model/BL/Manager.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using Microsoft.WindowsAzure.MobileServices;
+using Microsoft.WindowsAzure.MobileServices.Sync;
+using Microsoft.WindowsAzure.MobileServices.SQLiteStore;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace healthbook.Model.BL
 {
@@ -10,12 +15,108 @@ namespace healthbook.Model.BL
 	public class Manager
 	{
 		#region constant
-		public static readonly String APP_CONTEXT = "Manager";
+		public const string APP_CONTEXT = "Manager";
+		const string applicationURL = @"https://ifn661.azure-mobile.net/";
+		const string gatewayURL = @"https://ifn661.azure-mobile.net/";
+		const string applicationKey = @"wNGmbBuotheUnKbsLsXNpIfOrYIXyp87";
+		const string localDbPath    = "localstore.db";
 		#endregion
 
-		public Manager ()
-		{
+		#region var
+		private static Manager me = null;
+		private MobileServiceClient client;
+		private IMobileServiceSyncTable<Patient> patientTable;
+		public List<Patient> Items { get; private set;}
+
+		#endregion
+
+		#region singleton
+		public static Manager Instance {
+			get {
+				if(me == null)
+				{
+					me = new Manager();
+				}
+				return me;
+			}
 		}
+		#endregion
+
+		#region constructor
+		private Manager ()
+		{
+			client = new MobileServiceClient(applicationURL, applicationKey);
+			CurrentPlatform.Init ();
+			SQLitePCL.CurrentPlatform.Init(); 
+
+			patientTable = client.GetSyncTable <Patient> ();
+			Items = new List<Patient> ();
+
+		}
+		#endregion
+
+		#region init
+		public async Task InitializeStoreAsync()
+		{
+			var store = new MobileServiceSQLiteStore(localDbPath);
+			store.DefineTable<Patient>();
+
+			// Uses the default conflict handler, which fails on conflict
+			// To use a different conflict handler, pass a parameter to InitializeAsync. For more details, see http://go.microsoft.com/fwlink/?LinkId=521416
+			await client.SyncContext.InitializeAsync(store);
+		}
+		#endregion
+
+		#region synchronization
+		public async Task SyncAsync()
+		{
+			try
+			{
+				await client.SyncContext.PushAsync();
+				await patientTable.PullAsync("allPatientItems", patientTable.CreateQuery()); // query ID is used for incremental sync
+			}
+
+			catch (MobileServiceInvalidOperationException e)
+			{
+				Console.Error.WriteLine(@"Sync Failed: {0}", e.Message);
+			}
+		}
+
+		public async Task<List<Patient>> RefreshDataAsync ()
+		{
+			try {
+				// update the local store
+				// all operations on todoTable use the local database, call SyncAsync to send changes
+				await SyncAsync(); 							
+
+				// This code refreshes the entries in the list view by querying the local TodoItems table.
+				// The query excludes completed TodoItems
+				Items = await patientTable.ToListAsync ();
+				String wait= "fffff";
+			} catch (MobileServiceInvalidOperationException e) {
+				Console.Error.WriteLine (@"ERROR {0}", e.Message);
+				return null;
+			}
+
+			return Items;
+		}
+		#endregion
+
+		#region datamanipulation
+		public async Task InsertPatientItemAsync (Patient patientItem)
+		{
+			try {                
+				await patientTable.InsertAsync (patientItem); // Insert a new TodoItem into the local database. 
+				await SyncAsync(); // send changes to the mobile service
+
+				Items.Add (patientItem); 
+
+			} catch (MobileServiceInvalidOperationException e) {
+				Console.Error.WriteLine (@"ERROR {0}", e.Message);
+			}
+		}
+
+		#endregion
 	}
 }
 
--- healthbook/healthbook/ViewModel/DoctorOverviewViewModel.cs
+++ healthbook/healthbook/ViewModel/DoctorOverviewViewModel.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using Microsoft.WindowsAzure.MobileServices;
+using healthbook.Model.BL;
 
 namespace healthbook.ViewModel
 {
@@ -13,29 +15,22 @@ namespace healthbook.ViewModel
 		public DoctorOverviewViewModel ()
 		{
 			Patients = new List<Patient> ();
-			Patients.Add (new Patient () {
-				Name = "Mark",
-				Image = "http://www.fatosdesconhecidos.com.br/wp-content/uploads/2014/11/06162144983530.jpg"
-			});
-			Patients.Add (new Patient () {
-				Name = "Billy",
-				Image = "https://financialpostcom.files.wordpress.com/2012/12/1212gates.jpg"
-			});
-			Patients.Add (new Patient () {
-				Name = "Joker",
-				Image = "http://vignette2.wikia.nocookie.net/batman/images/4/49/MyCard_The_Joker.jpg"
-			});
-			Patients.Add (new Patient () {
-				Name = "Sheldon",
-				Image = "http://img1.wikia.nocookie.net/__cb20110725161640/bigbangtheory/de/images/5/52/Sheldons_Superblick.jpg"
-			});
+			initData ();
 
-			//TODO only for Tests
-			foreach (Patient p in Patients) {
-				p.GenerateRandomMeetings ();
-			}
 		}
 
+		 async void initData ()
+		{
+			await Manager.Instance.InitializeStoreAsync ();
+			refreshData ();
+		}
+
+		async void refreshData()
+		{
+			await Manager.Instance.RefreshDataAsync ();
+			Patients = Manager.Instance.Items;
+			RaisePropertyChanged ("Patients");
+		}
 	}
 }
 
