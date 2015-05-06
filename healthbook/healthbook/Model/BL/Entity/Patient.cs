183a2c9ccc0703e3a70ecb7ebcf2126de869928c
Atabak Sahraei <atabaksahraei@Atabaks-Macbook-Pro.local>
2015-05-06 17:11:27 +1000

    ListView With dynamic images from Web

--- healthbook/healthbook/Model/BL/Entity/Patient.cs
+++ healthbook/healthbook/Model/BL/Entity/Patient.cs
@@ -4,9 +4,13 @@ namespace healthbook
 {
 	public class Patient
 	{
+		public string Id { get; set; }
 		public string Name { get; set; }
+		public string Image { get; set; }
+
 		public Patient ()
 		{
+			Id = Guid.NewGuid ().ToString ();
 		}
 	}
 }
--- healthbook/healthbook/ViewModel/DoctorOverviewViewModel.cs
+++ healthbook/healthbook/ViewModel/DoctorOverviewViewModel.cs
@@ -9,10 +9,10 @@ namespace healthbook.ViewModel
 		public DoctorOverviewViewModel ()
 		{
 			Patients = new List<Patient> ();
-			Patients.Add (new Patient (){ Name = "Atabak" });
-			Patients.Add (new Patient (){ Name = "Jens" });
-			Patients.Add (new Patient (){ Name = "Jérome" });
-			Patients.Add (new Patient (){ Name = "Bonny" });
+			Patients.Add (new Patient (){ Name = "Mark", Image = "http://www.fatosdesconhecidos.com.br/wp-content/uploads/2014/11/06162144983530.jpg" });
+			Patients.Add (new Patient (){ Name = "Billy", Image = "https://financialpostcom.files.wordpress.com/2012/12/1212gates.jpg" });
+			Patients.Add (new Patient (){ Name = "Joker", Image = "http://vignette2.wikia.nocookie.net/batman/images/4/49/MyCard_The_Joker.jpg" });
+			Patients.Add (new Patient (){ Name = "Sheldon", Image="http://img1.wikia.nocookie.net/__cb20110725161640/bigbangtheory/de/images/5/52/Sheldons_Superblick.jpg" });
 		}
 	}
 }
