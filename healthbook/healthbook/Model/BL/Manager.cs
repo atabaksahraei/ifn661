9c399b5a9106ea4933da9be356b5ecdc65a30b54
Atabak Sahraei <atabaksahraei@Atabaks-Macbook-Pro.local>
2015-06-08 19:37:13 +1000

    background refresh each half secounds

--- healthbook/healthbook/Model/BL/Manager.cs
+++ healthbook/healthbook/Model/BL/Manager.cs
@@ -19,7 +19,7 @@ namespace healthbook.Model.BL
 		public const int BEACON_MAJOR_DOCTOR = 1;
 		public const string CONTEXT = "Manager";
 		public const bool TRACE = true;
-		public const string TMP_PATIENT_NAME = "Billy";
+		public const string TMP_PATIENT_NAME = "Atabak";
 		public const string TMP_DOCTOR_NAME = "Dr. Bonny";
 
 		#endregion
@@ -96,7 +96,7 @@ namespace healthbook.Model.BL
 				while(backgroundMode)
 				{
 					RefreshDataAsync();
-					Thread.Sleep(2000);
+					Thread.Sleep(500);
 					TraceManager.Trace(CONTEXT, Thread.CurrentThread.ManagedThreadId, "Background Sync");
 					if(Observer != null)
 					{
